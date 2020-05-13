2a5b467bc1215e9e6496acecb459493184d9f496
abhishek-mittal <abhishekmittaloffice@gmail.com>
2020-05-13 18:06:02 +0530

    Updates

---
+++ precache-manifest.f2049dac3b004ff93f01a75a5e4a7239.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fa38d41e6c0914276f5854d663ff369",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0597f9996a5b62403339",
+    "url": "/static/css/2.5caf27bd.chunk.css"
+  },
+  {
+    "revision": "0e52a794c4521a7dd45c",
+    "url": "/static/css/main.72b285d4.chunk.css"
+  },
+  {
+    "revision": "0597f9996a5b62403339",
+    "url": "/static/js/2.ef072012.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.ef072012.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e52a794c4521a7dd45c",
+    "url": "/static/js/main.21282c76.chunk.js"
+  },
+  {
+    "revision": "c3c5018b854d64cdc7f2",
+    "url": "/static/js/runtime-main.8dc0443e.js"
+  }
+]);
